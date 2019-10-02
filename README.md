fea7c1b093d246859c66802c5c8b6249a8f85e6e
Julie Jonak <juliejonak10@gmail.com>
2019-10-02 11:47:02 -0700

    Sets up env and tests

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# backend
-The Backend repo for Paw Prints, a TechCrunch Hackathon 2019 Project
+# Paw Prints Backend
+
+The live deploy can be found [here]() on Heroku.
+
+Uses Node.js with Express, integrating with Nationwide's API and the PetFinder API.
+
+## To Use
+
+Setup a `.env` file with the following variables:
+
+PORT = {Your chosen port number locally}
+PETFINDER_API_KEY = {Your Petfinder API Key}
+PETFINDER_SECRET = {Your Petfinder API Secret}
---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require("express");
+const server = express();
+const cors = require('cors');
+
+// If any required env variables are missing, exits
+if (!process.env.PETFINDER_API_KEY || !process.env.PETFINDER_SECRET || !process.env.PORT){
+    console.log("Error: ensure PETFINDER_API_KEY, PETFINDER_SECRET and PORT are defined in environment variables.")
+    process.exit(1);
+};
+
+// Sets env variables
+const PORT = process.env.PORT;
+const PETFINDER_API_KEY = process.env.PETFINDER_API_KEY;
+const PETFINDER_SECRET = process.env.PETFINDER_SECRET;
+
+server.use(express.json());
+server.use(cors());
+
+
+// SERVER
+
+server.listen(PORT, ()=>{
+    console.log(`Server is listening on ${PORT}`)
+});
