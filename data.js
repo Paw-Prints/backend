a51a8d39c864f13f1c729ce9b5a03a520e268683
Julie Jonak <juliejonak10@gmail.com>
2019-10-02 20:14:22 -0700

    Adds healthy data

--- data.js
+++ data.js
@@ -35,7 +35,7 @@ const sampleReturn = {
     ]
 };
 
-// Weight: pounds, food: cups per day, lifespan: years, healthIssues: top health defects and their associated cost
+// Weight: pounds, food: cups per day, lifespan: years, grooming: times per year, healthIssues: top health defects and their associated cost
 // Food based on this chart: https://www.petmd.com/blogs/nutritionnuggets/dr-coates/2015/july/are-you-feeding-your-dog-right-amount-32905
 
 // Average annual vet cost?
@@ -45,61 +45,71 @@ const dogBreeds = {
         weight: 67.5,
         food: 3.375,
         lifespan: 12,
-        healthIssues: ""
+        grooming: 6,
+        healthIssues: "Labs are susceptible to eye diseases and joint problems, like hip and elbow dysplasia which cost $1,700 - $4,500 to treat."
     },
     "German Shepherd Dog": {
         weight: 68.5,
         food: 3.425,
         lifespan: 11,
-        healthIssues: ""
+        grooming: 6,
+        healthIssues: "Hip and elbow dysplasia are health concerns, ranging from $1,700-$4,500 in surgical treatment."
     },
     "Golden Retriever": {
         weight: 65,
         food: 3.25,
         lifespan: 11,
-        healthIssues: ""
+        grooming: 6,
+        healthIssues: "Hip dysplasia and Von Willebrand Disease are common cocerns, ranging from $1,700-$4,500 in surgical treatment."
     },
     "French Bulldog": {
         weight: 22,
         food: 1.5,
         lifespan: 12,
-        healthIssues: ""
+        grooming: 6,
+        healthIssues: "Frequent respiratory issues, skin and ear diseases, and spinal disease leads to roughly $3,000-$6,000 in anticipated surgical costs."
     },
     "Australian Shepherd": {
         weight: 52.5,
         food: 2.67,
         lifespan: 14,
-        healthIssues: ""
+        grooming: 6,
+        healthIssues: "Hip and elbow dysplasia are health concerns, ranging from $1,700-$4,500 in surgical treatment."
     },
     "Beagle": {
         weight: 22,
         food: 1.5,
         lifespan: 27,
-        healthIssues: ""
+        grooming: 6,
+        healthIssues: "Beagles are generally a healthy breed. The most common health issue is cherry eye, about $300-$500 to treat surgically."
     },
     "Poodle": {
         weight: 57.5,
         food: 2.875,
         lifespan: 13.5,
-        healthIssues: ""
+        grooming: 12,
+        healthIssues: "Hip dysplasia ranges from $1,700-$4,500 in surgical treatment. Eye conditions and thyroid issues are common concerns."
     },
     "Siberian Husky": {
         weight: 47.5,
         food: 2.53,
         lifespan: 13.5,
-        healthIssues: ""
+        grooming: 6,
+        healthIssues: "10% of Huskies have cataracts or other eye diseases. Treatment costs roughly $2,700-$4,000."
     },
     "Yorkshire Terrier": {
         weight: 6,
         food: 0.5,
         lifespan: 14.5,
-        healthIssues: ""
+        grooming: 12,
+        healthIssues: "Hypoglycemia is frequent, with treatments from $1,000-$8,000. Legg-Perthes Disease and skin allergies are also common."
     },
     "Pembroke Welsh Corgi": {
         weight: 26.5,
         food: 1.55,
         lifespan: 13.5,
-        healthIssues: ""
+        grooming: 6,
+        healthIssues: "Hip dysplasia ranges from $1,700-$4,500 in surgical treatment. Lifelong eye conditions and Von Willebrand Disease are common concerns."
     }
 };
 
